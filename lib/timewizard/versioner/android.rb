f2c14023fc047b477625678379a50a76f1f092fe
Richard Harrah <topplethenunnery@gmail.com>
2015-08-17 11:23:35 -0400

    fixing bugs within find_version_name

--- lib/timewizard/versioner/android.rb
+++ lib/timewizard/versioner/android.rb
@@ -64,13 +64,16 @@ module Timewizard
 
       def find_version_code_and_name(container)
         parts = container.match("android:versionCode=\"(.*)\"")
-        version_code = parts[1]
-
-        @old_version_code = (version_code.to_i)
-        @new_version_code = (version_code.to_i + 1)
 
         text = parts[0]
-        text_split = text.partition(/\s/)
+        text_split = text.partition /\s+/
+
+        @old_version_code = Timewizard::Utils::Wizardry.to_i text_split[0]
+        @new_version_code = (@old_version_code + 1)
+
+        @old_version_name = Timewizard::Utils::Wizardry.only_semver text_split[2]
+        @new_version_name = @old_version_name
+
         codes = [text_split[0], text_split[2]]
       end
 
@@ -81,14 +84,27 @@ module Timewizard
           text = text.gsub(version, @new_version_code.to_s)
         else
           @new_version_code = change_to.to_i
-          text = text.gsub(version, change_to.to_s)
+          text = text.gsub(version, @new_version_code.to_s)
         end
       end
 
-      def change_manifest(container, change_to = '-1')
+      def change_version_name(parts, change_to = '-1')
+        text = parts
+        version = Timewizard::Utils::Wizardry.only_semver text
+        if change_to.to_s == '-1'
+          text = text.gsub(version, @new_version_name.to_s)
+        else
+          @new_version_name = Timewizard::Utils::Wizardry.only_semver change_to
+          text = text.gsub(version, @new_version_name.to_s)
+        end
+      end
+
+      def change_manifest(container, version_code_change = '-1', version_name_change = '-1')
         version_code = find_version_code(container)
-        changed_code = change_version_code(version_code, change_to)
-        container = container.gsub(version_code, changed_code)
+        changed_code = change_version_code(version_code, version_code_change)
+        version_name = find_version_name(container)
+        changed_name = change_version_name(version_name, version_name_change)
+        container = container.gsub(version_code, changed_code).gsub(version_name, changed_name)
       end
 
       def write_manifest(container)
