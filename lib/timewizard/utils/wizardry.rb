e453d6315f3ae67f4b3a51840d572e89b9f1ba06
Richard Harrah <topplethenunnery@gmail.com>
2015-08-19 14:56:43 -0400

    refactoring SEMVER_REGEX to VERSION_REGEX

--- lib/timewizard/utils/wizardry.rb
+++ lib/timewizard/utils/wizardry.rb
@@ -2,14 +2,14 @@ module Timewizard
   module Utils
     module Wizardry
 
-      SEMVER_REGEX = /(\d+\.\d+\.\d+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/
+      VERSION_REGEX = /^((\d+\.)?(\d+\.)?(\*|\d+))(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/
 
       def self.to_i(stringy)
         stringy.gsub(/\D/, '').to_i || 0
       end
 
       def self.only_version(stringy)
-        SEMVER_REGEX.match(stringy).to_s
+        VERSION_REGEX.match(stringy).to_s
       end
 
     end
