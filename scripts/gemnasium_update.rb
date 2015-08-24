271723c8753894390d6bbc63c8f9a55dbb34562c
Richard Harrah <topplethenunnery@gmail.com>
2015-08-24 18:49:30 -0400

    updating gemnasium_update.rb to use branches

--- scripts/gemnasium_update.rb
+++ scripts/gemnasium_update.rb
@@ -1,16 +1,8 @@
 #!/usr/bin/env ruby
 
-class Object
-  # Returns true if nil or empty.
-  # @return [Boolean] nil or empty
-  def blank?
-    respond_to?(:empty?) ? empty? : !self
-  end
-end
-
-if ENV['TRAVIS_TAG'].blank?
-  puts 'Not running gemnasium update'
-else
+if ENV.fetch('TRAVIS_BRANCH', 'development') == 'master'
   puts 'Running gemnasium update'
   `gemnasium autoupdate run`
+else
+  puts 'Not running gemnasium update'
 end
