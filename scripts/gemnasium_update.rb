89a27720babd72847d776dc6d259169510456fb2
Richard Harrah <topplethenunnery@gmail.com>
2015-09-11 14:07:16 -0400

    removing gemnasium_update.rb

--- scripts/gemnasium_update.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-if ENV.fetch('TRAVIS_BRANCH', 'development') == 'master'
-  puts 'Running gemnasium update'
-  `gemnasium autoupdate run`
-else
-  puts 'Not running gemnasium update'
-end
